39908796cca712db53a7cfca686412b84defce29
Nubdark <hokaidi58@gmail.com>
2022-09-08 12:07:40 -0700

    T4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-/**
-* main - print the string in the puts function
-* Description: using the main function
-* program that prints exactly "Programming is like building a multilingual puzzle
-* Return: 0
+/**                                                                                                                                     
+
+* main - print the string in the puts function                                                                                          
+
+* Description: using the main function                                                                                                  
+
+* program that prints exactly "Programming is like building a multilingual puzzle                                                       
+
+* Return: 0                                                                                                                             
+
 */
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
