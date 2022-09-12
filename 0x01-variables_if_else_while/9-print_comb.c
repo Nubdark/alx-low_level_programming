454f7e36db5950b961348fa60a23ecd170222b39
Nubdark <hokaidi58@gmail.com>
2022-09-12 10:32:32 -0700

    T9r1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,17 @@
  */
 int main(void)
 {
-int c;
-for (c = 48; c <= 57; c++)
-{
-	putchar(c);
-	if (c != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
