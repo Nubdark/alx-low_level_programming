173e0507951dbf3322ace0d34ad2e7c8edd23f53
Nubdark <hokaidi58@gmail.com>
2022-09-12 08:34:47 -0700

    T2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print function present in it
+*
+* Description: using the main function
+* this program prints the alphabet in lowercase
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
