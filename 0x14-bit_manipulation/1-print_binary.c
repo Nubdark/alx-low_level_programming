b18f119a01abc63f0f3a4d43188b1b0276f1c0aa
Nubdark <hokaidi58@gmail.com>
2022-10-31 04:13:32 -0700

    T1 change

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,10 +2,10 @@
 
 /**
  * print_binary - prints the binary representation
- * of a number
- * @n: unsigned long int
+ * of a number.
+ * @n: unsigned long int.
  *
- * Return: no return
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
