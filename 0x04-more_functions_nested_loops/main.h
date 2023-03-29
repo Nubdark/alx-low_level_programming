7c9169d25f9adf8a2f6857e02fe45d7a58f3fad1
Nubdark <110177130+Nubdark@users.noreply.github.com>
2023-03-29 15:50:35 +0300

    Update main.h
    maybe was the issue

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,23 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-/**
-*main - header file for prototypes
-*void prototypes (void)
-*int prototypes (int)
-*/
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int_n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
