36563ddbb0f8ac699a269c4f85dc8b39b921d96d
Nubdark <110177130+Nubdark@users.noreply.github.com>
2023-03-30 14:15:32 +0300

    updated main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n)
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
@@ -22,4 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
