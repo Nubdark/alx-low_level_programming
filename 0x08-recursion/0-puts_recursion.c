c5c59c7791bb3ec9e60b1d34892a19c235ee4c9f
Nubdark <110177130+Nubdark@users.noreply.github.com>
2023-06-22 15:43:21 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
-* _puts_recursion - prints's a string followed by a new line
+ * _puts_recursion - Print a string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
